1daaf1e7949b5aac8dc4743ca609066b0a98a5fd
samnevers <thesamanthaki@gmail.com>
2017-02-04 18:38:44 +1100

    redesign initial commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 console.log("Yo");
+console.log(innerWidth);
+var dist = window.innerWidth;
+dist += "px";
+// var potatoAnimation = null;
+var rollAnimations = function () {
+  var $potato = $(".potato");
+  TweenMax.to($potato, 5, {
+    rotation: 2600,
+    left: dist,
+    ease: Linear.easeNone,
+    repeat: -1,
+    repeatDelay: 0.5,
+    yoyo: true,
+    xPercent: window.innerWidth * 2.4
+  }, 1.5);
+};
+
+$(document).ready(function() {
+  // $(".potato").on("click", rollAnimations);
+  console.log( dist );
+  rollAnimations();
+
+  // $(".project-one").hover( function () {
+  //   $(this).append( $("<img src='assets/project-1.jpg'>"));
+  // });
+});
